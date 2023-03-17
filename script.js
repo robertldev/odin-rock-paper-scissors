d4bc68b85a96535ad9fbb046b2250f28e7a408ab
Rob <127467383+robertldev@users.noreply.github.com>
2023-03-17 14:29:28 +1100

    Update to Rock Paper Scissors game
    Generated 5-round game which ascertains a winner.

--- script.js
+++ script.js
@@ -24,27 +24,54 @@ function playRound(playerSelection, computerSelection) {
         return "This round is a tie!"
     }
     else if (playerCompare == "rock" && computerCompare == "scissors") {
+        playerScore++;
         return "You win! Rock beats scissors!"
     }
     else if (playerCompare == "rock" && computerCompare == "paper") {
+        computerScore++;
         return "You lose! Paper beats rock!"
     }
     else if (playerCompare == "paper" && computerCompare == "rock") {
+        playerScore++;
         return "You win! Paper beats rock!"
     }
     else if (playerCompare == "paper" && computerCompare == "scissors") {
+        computerScore++;
         return "You lose! Scissors beats paper!"
     }
     else if (playerCompare == "scissors" && computerCompare == "paper") {
+        playerScore++;
         return "You win! Scissors beats paper!"
     }
     else if (playerCompare == "scissors" && computerCompare == "rock") {
+        computerScore++;
         return "You lose! Rock beats scissors!"
     }
 }
 
 /* Creating a 5-round game of Rock Paper Scissors */
 
-let playerSelection = 'scissors';
-let computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please enter a choice - rock, paper or scissors");
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+ }
+
+ /* Check winner */
+
+function findWinner() {
+    if (playerScore == computerScore) {
+        return "The game was a tie."
+    } else if (playerScore > computerScore) {
+        return "You won the game!"
+    } else if (computerScore > playerScore) {
+        return "You lost the game."
+    }
+}
+
+console.log("You scored " + playerScore);
+console.log("The computer score " + computerScore);
+console.log(findWinner());
